19e6e0d42ca3ec4db540d90a56be9b6351255f8c
Divya Moncy <divyaamoncy@gmail.com>
2022-01-05 16:41:30 +0530

    Refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BorrowersignupComponent } from './borrowersignup/borrowersignup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-
+  { path: 'borrowersignup', component: BorrowersignupComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
