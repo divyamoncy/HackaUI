5296a58c96563f3f9d8ef8d200e5382921fac8f1
Divya Moncy <divyaamoncy@gmail.com>
2022-01-11 17:49:46 +0530

    File upload and refer

--- src/app/borroweronboarding/borroweronboarding.component.ts
+++ src/app/borroweronboarding/borroweronboarding.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BorroweronboardingComponent implements OnInit {
   public focus: number;
   constructor() {
-    this.focus = 2;
+    this.focus = 4;
   }
 
   ngOnInit(): void {
